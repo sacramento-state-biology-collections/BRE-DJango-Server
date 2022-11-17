a6caacab0953007135bde360e8185e1657b5f9b9
May, Joseph <josephmay@csus.edu>
2022-11-16 17:25:34 -0800

    rest api creates json for all entries and 1 search

--- rest.py
+++ rest.py
@@ -2,17 +2,33 @@ from flask import Flask
 import pandas as pd
 import json
 
-jsonFilePath = "./rest.json"
 primaryKey = "Catalog ."
+searchableColumns = ['Common Name']
+
+def sortData(df, column, search):
+    return df.where(df[column] == search )
+
 
 app = Flask(__name__)
 
 @app.route('/api/v1/<collection>', methods=['GET'])
-def tabledata(collection):
+def alldata(collection):
     if collection == 'all':
         pass #add logic for all csvs later
     csvFilePath = f'./data/{collection}.csv'
     df = pd.read_csv(csvFilePath)
+    parsed = json.loads(df.to_json())
+    return parsed
+
+@app.route('/api/v1/<collection>/<search>', methods=['GET'])
+def showdata(collection, search):
+    if collection == 'all':
+        pass #add logic for all csvs later
+    csvFilePath = f'./data/{collection}.csv'
+    df = pd.read_csv(csvFilePath)
+    for x in searchableColumns:
+        df = sortData(df, x, search)
+    df = df.dropna(how = 'all')
     parsed = json.loads(df.to_json())
     return parsed
 
